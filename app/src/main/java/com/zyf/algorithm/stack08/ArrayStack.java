9c65f24539e36cf28d4f68bf8f13e6acdce02566
zhengyf <zhengyf@chinaoly.com>
2019-03-22 18:25:25 +0800

    添加练习代码

--- app/src/main/java/com/zyf/algorithm/stack08/ArrayStack.java
+++ app/src/main/java/com/zyf/algorithm/stack08/ArrayStack.java
@@ -1,5 +1,7 @@
 package com.zyf.algorithm.stack08;
 
+import java.util.Arrays;
+
 /**
  * 使用数组实现栈
  */
@@ -31,5 +33,26 @@ public class ArrayStack {
         String item = items[count - 1];
         count--;
         return item;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(5);
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+        stack.push("5");
+        printStack(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        stack.push("6");
+        printStack(stack);
+    }
+
+    private static void printStack(ArrayStack stack) {
+        if (stack.items == null) {
+            return;
+        }
+        System.out.println(Arrays.toString(stack.items));
     }
 }
